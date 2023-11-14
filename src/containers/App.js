f5c16d14e5baa387f1f15cf446a09d05d455bde8
dimicodes <dkmiamib@gmail.com>
2023-11-14 15:29:57 +0100

    11/14

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from "../components/ErrorBoundary";
 
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
                     <h1 className="f1">ROBOFRIENDS</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
+                        <ErrorBoundary>
                         <CardList robots= {filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
